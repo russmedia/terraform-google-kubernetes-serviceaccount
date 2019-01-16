608399bb153954478011e009b31f2ce028e28099
Eryk Zalejski <ezalejski@gmail.com>
2019-01-16 15:22:51 +0100

    initial setup

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "serviceaccount_id" {
+  value = "${google_service_account.serviceaccount.unique_id}"
+}
+
+output "serviceaccount_private_key" {
+  value = "${google_service_account_key.account_key.private_key}"
+}
+
+output "serviceaccount_email" {
+  value = "${google_service_account.serviceaccount.email}"
+}
